dac15b61a6359f87acfa4f6761af2cbe9b150303
tomei <>
2010-07-27 18:29:56 +0000

    *** empty log message ***

---
+++ RSGraviton/RSAnalyzer/python/Spring10/RSWWjjlnu_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring() 
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend( [
+    'file:/storage/trtomei/data/Pythia_GWWjjlnu_800GeV_kmpl005_CMSSW358_RECO.root'
+    ] );
+
+secFiles.extend( [
+               ] )
+
