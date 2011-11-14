008cdbb63cda88f2c8f44bf5db8d56e7d3d20098
tomei <>
2011-11-14 14:27:51 +0000

    *** empty log message ***

---
+++ RSGraviton/RSAnalyzer/python/PFjethistos_cff.py
@@ -0,0 +1,79 @@
+import FWCore.ParameterSet.Config as cms
+
+histograms = cms.VPSet(cms.PSet(
+# Basic kinematics
+    nbins = cms.untracked.int32(100),
+    description = cms.untracked.string('jet_%d_et'),
+    plotquantity = cms.untracked.string('et'),
+    min = cms.untracked.double(0.0),
+    max = cms.untracked.double(1000.0),
+    itemsToPlot = cms.untracked.int32(4),
+    name = cms.untracked.string('jet_%d_et')
+    ), 
+    cms.PSet(
+        nbins = cms.untracked.int32(100),
+        description = cms.untracked.string('jet_%d_eta'),
+        plotquantity = cms.untracked.string('eta'),
+        min = cms.untracked.double(-4.0),
+        max = cms.untracked.double(4.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_eta')
+    ), 
+    cms.PSet(
+        nbins = cms.untracked.int32(100),
+        description = cms.untracked.string('jet_%d_pt'),
+        plotquantity = cms.untracked.string('pt'),
+        min = cms.untracked.double(0.0),
+        max = cms.untracked.double(1000.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_pt')
+    ), 
+# Jet mass
+     cms.PSet(
+        nbins = cms.untracked.int32(120),
+        description = cms.untracked.string('jet_%d_mass'),
+        plotquantity = cms.untracked.string('mass'),
+        min = cms.untracked.double(0.0),
+        max = cms.untracked.double(300.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_mass')
+        ),
+# PF Jet ID
+     cms.PSet(
+        nbins = cms.untracked.int32(20),
+        description = cms.untracked.string('jet_%d_chf'),
+        plotquantity = cms.untracked.string('chargedHadronEnergyFraction'),
+        min = cms.untracked.double(0.0),
+        max = cms.untracked.double(1.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_chf')
+        ),
+     cms.PSet(
+        nbins = cms.untracked.int32(20),
+        description = cms.untracked.string('jet_%d_nhf'),
+        plotquantity = cms.untracked.string('neutralHadronEnergyFraction'),
+        min = cms.untracked.double(0.0),
+        max = cms.untracked.double(1.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_nhf')
+        ),
+     cms.PSet(
+        nbins = cms.untracked.int32(20),
+        description = cms.untracked.string('jet_%d_cef'),
+        plotquantity = cms.untracked.string('chargedEmEnergyFraction'),
+        min = cms.untracked.double(0.0),
+        max = cms.untracked.double(1.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_cef')
+        ),
+     cms.PSet(
+        nbins = cms.untracked.int32(120),
+        description = cms.untracked.string('jet_%d_nef'),
+        plotquantity = cms.untracked.string('neutralEmEnergyFraction'),
+        min = cms.untracked.double(0.0),
+        max = cms.untracked.double(1.0),
+        itemsToPlot = cms.untracked.int32(4),
+        name = cms.untracked.string('jet_%d_cef')
+        )
+                       )
+
